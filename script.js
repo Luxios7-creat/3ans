5e6966ae00232e79d656a426e7a2ea1d55cc07ad
Luxios7 <leotaurin7@gmail.com>
2026-03-11 09:42:41 +0100

    Mise ├á jour du projet Wed 03/11/2026  9:42:41.06

--- script.js
+++ script.js
@@ -3,5 +3,3 @@ const list = document.getElementById("entries-list");
 const chartCtx = document.getElementById("timeChart").getContext("2d");
 const periodSelect = document.getElementById("period");
 const exportBtn = document.getElementById("exportBtn");
-
-let entries = JSON.parse(localStorage.getItem("entries")) || [];
